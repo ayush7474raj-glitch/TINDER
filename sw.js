ae64fd88018c95b85c0e20a58fbc0af87cf5a9c1
ayush7474raj-glitch <ayush7474raj@gmail.com>
2026-07-26 03:06:39 +0530

    Fix icon paths in service worker

--- sw.js
+++ sw.js
@@ -7,8 +7,8 @@ const FILES_TO_CACHE = [
   "/app.js",
   "/firebase.js",
   "/manifest.json",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
+  "/icon-192.png",
+  "/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
